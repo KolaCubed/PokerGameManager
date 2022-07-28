5e44a582fe5a779c306c30a0505b6b2ae4949e60
Toby Kolk <tobias.k@boolooloo.network>
2022-07-29 00:39:59 +0100

    Start of Blimds

---
+++ PokerGameManager/PokerGameManager/PokerGameManager/Models/Blinds.cs
@@ -0,0 +1,78 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerGameManager.Models
+{
+    public class Blinds
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        IEnumerable<Blind> BlindItems {get;set;}
+
+        public class Blind
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public int BlindsId { get; set; }
+            public int Level { get; set; }
+            public int Small { get; set; }
+            public int Big { get; set; }
+        }
+
+        public static bool InsertBlinds(Blinds blinds)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                {
+                    conn.CreateTable<Blinds>();
+                    conn.CreateTable<Blind>();
+
+                    foreach (var item in conn.Table<Blinds>().ToList())
+                    {
+                        if (item.BlindItems.Any())
+                        {
+                            var id = conn.Insert(blinds);
+
+                            foreach (var blind in item.BlindItems)
+                            {
+                                conn.Insert(blind);
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static IEnumerable<Blinds> GetBlinds()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Blinds>();
+                conn.CreateTable<Blind>();
+
+                var blinds = conn.Table<Blinds>().ToList();
+                var blindItems = conn.Table<Blind>().ToList();
+
+                foreach (var item in blinds)
+                {
+                    item.BlindItems = conn.Table<Blind>().Where(o => o.BlindsId == item.Id).ToList();
+                    blinds.Add(item);
+                }
+
+                return blinds;
+            }
+        }
+    }
+}
---
+++ PokerGameManager/PokerGameManager/PokerGameManager/ViewModels/BlindsVM.cs
@@ -0,0 +1,25 @@
+﻿using PokerGameManager.Models;
+using System.Collections.ObjectModel;
+
+namespace PokerGameManager.ViewModels
+{
+    public class BlindsVM
+    {
+        public ObservableCollection<Blinds> Blinds { get; set; }
+
+        public BlindsVM()
+        {
+            Blinds = new ObservableCollection<Blinds>();
+
+            GetBlinds();
+        }
+
+        public void GetBlinds()
+        {
+            foreach (var item in Models.Blinds.GetBlinds())
+            {
+                Blinds.Add(item);
+            }
+        }
+    }
+}
